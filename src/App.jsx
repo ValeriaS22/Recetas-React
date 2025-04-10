d1abf1ca29f4496443841ed24cc32c3ff6894ac0
Jose Navarro <jfnr398@gmail.com>
2025-04-10 18:59:45 -0500

    feat: se crea clone de divinacocina.es en react + vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import Header from "./componentes/header"
 import Menu from "./componentes/menu"
 import Aside from "./componentes/aside"
-import Content from "./componentes/content"
+import ContainerContent from "./componentes/containerContent"
 import Footer from "./componentes/footer"
 
 function App() {
 
   return (
     <>
-      <Header/>
-      <Menu/>
-      <div class="container mt-5">
-        <div class="row">
-          <Aside/>
-          <Content/>
+      <div className="container-principal">
+        <Header/>
+        <Menu/>
+        <div className="container mt-5" >
+          <div className="row">
+            <ContainerContent/>
+            <Aside/>
+          </div>
+          
         </div>
       </div>
       <Footer/>
--- src/componentes/content.jsx
+++ src/componentes/content.jsx
@@ -1,17 +1,66 @@
 export default function Content() {
   return (
-    <div className="col-sm-8">
-      <h2>TITLE HEADING</h2>
-      <h5>Title description, Dec 7, 2020</h5>
-      <div className="fakeimg">Fake Image</div>
-      <p>Some text..</p>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    <div className="col-sm-11 text-center content" style={{"margin": "50px 0"}}>
+      <div className="penci-page-header">
+        <h1 className="entry-title">Divina Cocina – Recetas  Fáciles</h1>
+      </div>
+      <div className="fakeimg">
+        <img src="https://www.divinacocina.es/wp-content/uploads/2025/04/sepia-salteada-con-guisantes-c.jpg" class="attachment-penci-full-thumb size-penci-full-thumb wp-post-image entered lazyloaded" alt="Sepia salteada con guisantes y verduras 1" data-lazy-srcset="https://www.divinacocina.es/wp-content/uploads/2025/04/sepia-salteada-con-guisantes-c.jpg 780w, https://www.divinacocina.es/wp-content/uploads/2025/04/sepia-salteada-con-guisantes-c-300x300.jpg 300w, https://www.divinacocina.es/wp-content/uploads/2025/04/sepia-salteada-con-guisantes-c-150x150.jpg 150w, https://www.divinacocina.es/wp-content/uploads/2025/04/sepia-salteada-con-guisantes-c-768x768.jpg 768w, https://www.divinacocina.es/wp-content/uploads/2025/04/sepia-salteada-con-guisantes-c-585x585.jpg 585w" data-lazy-sizes="(max-width: 780px) 100vw, 780px" title="Sepia salteada con guisantes y verduras 2" data-lazy-src="https://www.divinacocina.es/wp-content/uploads/2025/04/sepia-salteada-con-guisantes-c.jpg" data-ll-status="loaded" sizes="(max-width: 780px) 100vw, 780px" srcset="https://www.divinacocina.es/wp-content/uploads/2025/04/sepia-salteada-con-guisantes-c.jpg 780w, https://www.divinacocina.es/wp-content/uploads/2025/04/sepia-salteada-con-guisantes-c-300x300.jpg 300w, https://www.divinacocina.es/wp-content/uploads/2025/04/sepia-salteada-con-guisantes-c-150x150.jpg 150w, https://www.divinacocina.es/wp-content/uploads/2025/04/sepia-salteada-con-guisantes-c-768x768.jpg 768w, https://www.divinacocina.es/wp-content/uploads/2025/04/sepia-salteada-con-guisantes-c-585x585.jpg 585w"/>
+      </div>
+      <h2 style={{"text-align":"center", "margin-bottom":"7px" , "color": "#F35B2C !important"}}>
+        <a href="https://www.divinacocina.es/sepia-salteada-con-guisantes-y-verduras/">Sepia salteada con guisantes y verduras</a>
+      </h2>
+      
+      <span style={{ textAlign: 'center', margin: '20px 0' }}>
+        <div className="snippet-data">
+          <span className="star-img snippet-data-list xxxx">
+            <img
+              width="16"
+              height="16"
+              src="https://www.divinacocina.es/wp-content/plugins/all-in-one-schemaorg-rich-snippets/images/1star.png"
+              alt="1star"
+              title="Divina Cocina – Recetas Fáciles 123"
+            />
+            <img
+              width="16"
+              height="16"
+              src="https://www.divinacocina.es/wp-content/plugins/all-in-one-schemaorg-rich-snippets/images/1star.png"
+              alt="1star"
+              title="Divina Cocina – Recetas Fáciles 123"
+            />
+            <img
+              width="16"
+              height="16"
+              src="https://www.divinacocina.es/wp-content/plugins/all-in-one-schemaorg-rich-snippets/images/1star.png"
+              alt="1star"
+              title="Divina Cocina – Recetas Fáciles 123"
+            />
+            <img
+              width="16"
+              height="16"
+              src="https://www.divinacocina.es/wp-content/plugins/all-in-one-schemaorg-rich-snippets/images/1star.png"
+              alt="1star"
+              title="Divina Cocina – Recetas Fáciles 123"
+            />
+            <img
+              width="16"
+              height="16"
+              src="https://www.divinacocina.es/wp-content/plugins/all-in-one-schemaorg-rich-snippets/images/1star.png"
+              alt="1star"
+              title="Divina Cocina – Recetas Fáciles 123"
+            />
+          </span>
+        </div>
+        <br />
+        Basado en <b>1</b> opiniones.
+      </span>
 
-      <h2 className="mt-5">TITLE HEADING</h2>
-      <h5>Title description, Sep 2, 2020</h5>
-      <div className="fakeimg">Fake Image</div>
-      <p>Some text..</p>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+
+      <div className="col-lg-6">
+
+      </div>
+
+
     </div>
   )
 }
